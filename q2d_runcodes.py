fcfd998edca7b1baf89bd3bbcbbfe15e68f0d495
Noemy166 <69639823+Noemy166@users.noreply.github.com>
2020-08-17 10:15:40 -0300

    Update q2d_runcodes.py

--- q2d_runcodes.py
+++ q2d_runcodes.py
@@ -1,8 +1,15 @@
-ah = int(input())
-bm = int(input())
-cs = int(input())
-horas = ah * 3600
-minutos = bm * 60
-segundos = cs  
-resultado = horas + minutos + segundos
-print(f' {resultado}')
+def quest2_d():
+  return
+
+def main():
+  ah = int(input())
+  bm = int(input())
+  cs = int(input())
+  horas = ah * 3600
+  minutos = bm * 60
+  segundos = cs  
+  resultado = horas + minutos + segundos
+  print(f' {resultado}')
+  
+if __name__ == '__main__':
+  main()  
