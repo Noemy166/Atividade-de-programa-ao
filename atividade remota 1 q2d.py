298063178ac0d234d80658dc6779bb2f08cd2711
Noemy166 <69639823+Noemy166@users.noreply.github.com>
2020-08-17 09:59:31 -0300

    Update atividade remota 1 q2d.py

--- atividade remota 1 q2d.py
+++ atividade remota 1 q2d.py
@@ -1,8 +1,16 @@
-ah = int(input('Digite a hora: '))
-bm = int(input('Digite os minutos: '))
-cs = int(input('Digite os segundos: '))
-horas = ah * 3600
-minutos = bm * 60
-segundos = cs  
-resultado = horas + minutos + segundos
-print(f'Se passaram {resultado} segundos desde a meia-noite')
+def quest2_d():
+  return
+
+def main(): 
+  ah = int(input('Digite a hora: '))
+  bm = int(input('Digite os minutos: '))
+  cs = int(input('Digite os segundos: '))
+  horas = ah * 3600
+  minutos = bm * 60
+  segundos = cs  
+  resultado = horas + minutos + segundos
+  print(f'Se passaram {resultado} segundos desde a meia-noite')
+
+  
+if __name__ == '__main__':
+  main()
