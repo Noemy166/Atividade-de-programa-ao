8a38d44fd131bfb58ad3cafde21b731fbd42f6d3
Noemy166 <69639823+Noemy166@users.noreply.github.com>
2020-08-17 09:53:21 -0300

    Update atividade remota 1 q2c.py

--- atividade remota 1 q2c.py
+++ atividade remota 1 q2c.py
@@ -1,3 +1,11 @@
-a = int(input('Digite um número: '))
-resultado = a ** 3
-print(f'O resultado é: {resultado}')
+  
+def quest2_c():
+  return
+  
+def main():  
+  a = int(input('Digite um Número: '))
+  cubo = a**3
+  print(f'O número elevado ao cubo é {cubo}')
+  
+if __name__ == '__main__':
+  main()
