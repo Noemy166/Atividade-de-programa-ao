db370035054bf21584ea153aa7d9a3c2eedb3b7e
Noemy166 <69639823+Noemy166@users.noreply.github.com>
2020-08-17 09:50:09 -0300

    Update atividade remota 1 q2.b.py

--- atividade remota 1 q2.b.py
+++ atividade remota 1 q2.b.py
@@ -1,5 +1,12 @@
-a = int(input('Digite um número: '))
-b = int(input('Digite um número: '))
-c = int(input('Digite um número: '))
-media = (a + b + c)/3
-print(f'A media dos números é {media:.2f}')
+def quest2_b():
+  return
+  
+def main():  
+  a = int(input('Digite um número: '))
+  b = int(input('Digite um número: '))
+  c = int(input('Digite um número: '))
+  media = (a + b + c)/3
+  print(f'A media dos números é {media:.2f}')
+  
+if __name__ == '__main__':
+  main()  
